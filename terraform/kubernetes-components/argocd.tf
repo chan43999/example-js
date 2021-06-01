e28c942d4bd22adc071884b8bf4444e92622400e
Chan Suttichujit <chan.suttichujit@allianz.com>
2021-06-01 18:38:48 +0700

    Creat terraform and charts

--- terraform/kubernetes-components/argocd.tf
+++ terraform/kubernetes-components/argocd.tf
@@ -1,9 +1,24 @@
+locals {
+
+  value_files = {
+    server = {
+      config = {
+        repositories = yamlencode([
+          {
+            url = var.repository
+          }
+        ])
+      }
+    }
+  }
+}
 resource "helm_release" "argocd" {
   repository = "https://argoproj.github.io/argo-helm"
   chart = "argo-cd"
   name = "argocd"
   version = "3.1.1"
   namespace = kubernetes_namespace.argocd.metadata[0].name
+  values = [yamlencode(local.value_files)]
 }
 
 resource "kubernetes_namespace" "argocd" {
